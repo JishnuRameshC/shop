2818a8ea3d068b5f566887cc7b0006cb921b69fa
jishnu ramesh c <jishnurameshc@gmail.com>
2023-06-01 17:49:58 +0530

    second

--- app1/views.py
+++ app1/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 
 
 def home(request):
     return render(request,"app1/product.html")
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request,'app1/product_list.html',{'products':products})
